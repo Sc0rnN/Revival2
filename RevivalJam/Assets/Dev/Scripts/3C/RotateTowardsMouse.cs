1b81c9074fba4557cf0a3ff0c5c6f88da0ce9ad0
loudjit <ernestilouis@gmail.com>
2024-10-19 16:17:45 +0200

    déplacement + mousse ok

--- RevivalJam/Assets/Dev/Scripts/3C/RotateTowardsMouse.cs
+++ RevivalJam/Assets/Dev/Scripts/3C/RotateTowardsMouse.cs
@@ -8,6 +8,9 @@ public class RotateTowardsMouseAndMove : MonoBehaviour
     [SerializeField] private float velocity = 5f;
 
     private Rigidbody rb;
+    public int PlayerMovementSpeed = 5;
+    public int PlayerMovementSpeedWindy = 10;
+
 
     void Start()
     {
@@ -16,8 +19,9 @@ public class RotateTowardsMouseAndMove : MonoBehaviour
 
     void Update()
     {
-        RotateObjectTowardsMouse();
-        MoveObjectForward();
+        //RotateObjectTowardsMouse();
+        //MoveObjectForward();
+        MovePlayer();
     }
 
     void RotateObjectTowardsMouse()
@@ -40,5 +44,28 @@ public class RotateTowardsMouseAndMove : MonoBehaviour
     void MoveObjectForward()
     {
         rb.linearVelocity = transform.forward * velocity;
+    }
+
+    public void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal") ;
+        float verticalInput = 1f;
+        float movementSpeed;
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 player_Direction = transform.right * horizontalInput + transform.forward * verticalInput;
+        Quaternion targetRotation = Quaternion.LookRotation(player_Direction);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = PlayerMovementSpeedWindy;
+        }
+        else
+        {
+            movementSpeed = PlayerMovementSpeed;
+        }
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+
     }
 }
