1fbf312de49b662cd023df6bb841a14e7dec85a8
Niniutt <72707364+Niniutt@users.noreply.github.com>
2024-10-20 16:10:48 +0200

    Ajout musique dans menu

--- RevivalJam/Assets/Dev/Scripts/UI/MainMenu.cs
+++ RevivalJam/Assets/Dev/Scripts/UI/MainMenu.cs
@@ -6,6 +6,15 @@ public class MainMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] GameObject mainMenu;
+    [SerializeField] private AudioClip clip;
+
+    private void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+        audio.clip = clip;
+        audio.Play();
+    }
 
     public void Play(int sceneId)
 
